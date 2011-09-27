c2f8efa375166a4f3c0dc540e6880b17f65f1469
Phil Haeusler <philhaeusler@gmail.com>
2011-09-27 11:11:24 +1000

    Made build scripts generic

--- AirDBC/bin/package.bat
+++ AirDBC/bin/package.bat
@@ -1,2 +1,5 @@
-copy "P:\external\FREImageProcessor\FREImageProcessor\Debug\FREImageProcessor.dll" .\
-"O:\Applications\Adobe Flash Builder 4.5\sdks\4.5.1 + AIR 3.0\bin\adt.bat" -package -storetype pkcs12 -keystore test.p12 -storepass test -target ane ImageProcessor.ane extension.xml -swc ImageProcessor.swc -platform Windows-x86 library.swf FREImageProcessor.dll
+echo Replace SDK_PATH before running
+pause
+
+copy ..\..\AirDBCExtension/Debug/AirDBCExtension.dll . .\
+"SDK_PATH\bin\adt.bat" -package -storetype pkcs12 -keystore ../certs/test.p12 -storepass test -target ane AirDBC.ane extension.xml -swc AirDBC.swc -platform Windows-x86 library.swf AirDBCExtension.dll
--- AirDBC/bin/package.sh
+++ AirDBC/bin/package.sh
@@ -1,4 +1,5 @@
 #
+read -p "Replace SDK_PATH before running"
 echo Starting build of AirDBC.swc - `date`
 cp ../../AirDBCExtension/Debug/AirDBCExtension.dll .
-/Applications/Adobe\ Flash\ Builder\ 4.5/sdks/4.5.1\ +\ AIR\ 3.0/bin/adt -package -storetype pkcs12 -keystore ../certs/test.p12 -storepass test -target ane AirDBC.ane extension.xml -swc AirDBC.swc -platform Windows-x86 library.swf AirDBCExtension.dll
+SDK_PATH/bin/adt -package -storetype pkcs12 -keystore ../certs/test.p12 -storepass test -target ane AirDBC.ane extension.xml -swc AirDBC.swc -platform Windows-x86 library.swf AirDBCExtension.dll
